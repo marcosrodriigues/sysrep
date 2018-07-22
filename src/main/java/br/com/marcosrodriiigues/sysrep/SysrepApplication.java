9e848caa97afa5def1375896ac02976dbf3d2d44
marcosrodriigues <marcos.rodriiigues@gmail.com>
2018-07-21 23:38:11 -0300

    Hello, SysRep!

---
+++ src/main/java/br/com/marcosrodriiigues/sysrep/SysrepApplication.java
@@ -0,0 +1,12 @@
+package br.com.marcosrodriiigues.sysrep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysrepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysrepApplication.class, args);
+	}
+}
